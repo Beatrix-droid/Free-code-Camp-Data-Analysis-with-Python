1434254245a634624993af6a58007a8c33813586
Beatrice <amilcare8@gmail.com>
2023-03-30 15:09:03 +0100

    udpated bx plot group by function

--- time-series-visualiser.py
+++ time-series-visualiser.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-from pandas.plotting import register_matplotlib_converters
 import numpy as np
+from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
@@ -15,7 +15,6 @@ df=df[(df['value'] >= (df['value'].quantile(0.025))) & (df['value'] <= (df['valu
 
 
 def draw_line_plot():
-    # Draw line plot
     ypoints = np.array(df['value'])
     fig=plt.figure(figsize=(15,7))
   
@@ -25,16 +24,14 @@ def draw_line_plot():
     plt.xlabel('Date')
     plt.ylabel('Page Views')
 
-
-
-
-
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
     return fig
+    # Draw line plot
+
 
 def draw_bar_plot():
-    # Copy and modify data for monthly bar plot
+   # Copy and modify data for monthly bar plot
     df_bar = df.copy()
     df_bar.reset_index(inplace=True)
     #caprsint the date time objects and sorting teh months and columns
@@ -54,14 +51,12 @@ def draw_bar_plot():
     fig.set(xlabel='Average Page Views', ylabel="Years")
     plt.legend()
 
-
-
     # Save image and return fig (don't change this part)
     fig.figure.savefig('bar_plot.png')
     return fig
 
 def draw_box_plot():
-    # Prepare data for box plots (this part is done!)
+ # Prepare data for box plots (this part is done!)
     df_box = df.copy()
     df_box.reset_index(inplace=True)
     df_box['year'] = [d.year for d in df_box.date]
@@ -71,7 +66,7 @@ def draw_box_plot():
 
     # Draw box plots (using Seaborn)
         #grouping the dataframe as before
-    new_df =df_box.groupby(["year", "month","num"])["value"].mean().to_frame(name = 'count').reset_index()
+    new_df=df_box.groupby(["year", "month","num"],group_keys=True)["value"].apply(lambda x: x).to_frame(name = 'count').reset_index()
 
     #sorting the months
     sorted_df = new_df.sort_values(by=['num'], ascending=True)
